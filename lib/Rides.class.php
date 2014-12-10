871873751bad651ef0a55198cac62f60553263f8
jrfoell <jrfoell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-10 03:45:31 +0000

    Version 3 API
    git-svn-id: https://plugins.svn.wordpress.org/wp-strava/trunk@1041641 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/Rides.class.php
+++ lib/Rides.class.php
@@ -3,152 +3,51 @@
  * Rides is a class wrapper for the Strava REST API functions.
  */
 class WPStrava_Rides {
-	private $rideUrl = "http://www.strava.com/api/v1/rides/:id";
-	private $rideUrlV2 = "http://www.strava.com/api/v2/rides/:id";
-	private $ridesUrl = "http://www.strava.com/api/v1/rides";
-	private $authenticationUrl = "https://www.strava.com/api/v1/authentication/login";
-	private $authenticationUrlV2 = "https://www.strava.com/api/v2/authentication/login";
-	private $rideMapDetailsUrl = "http://www.strava.com/api/v1/rides/:id/map_details";
-	private $rideMapDetailsUrlV2 = "http://www.strava.com/api/v2/rides/:id/map_details";
-	
-	public $ridesLinkUrl = "http://app.strava.com/rides/";
-	public $athletesLinkUrl = "http://app.strava.com/athletes/";
-	public $stravaRides;
-	public $feedback;
-	
-	public function __construct() {
-		// Empty constructor
-	} // __construct
-	
-	public function getRideDetails($rideId, $systemOfMeasurement) {
-		$url = preg_replace('/:id/', $rideId, $this->rideUrl);
-		$json = file_get_contents($url);
 
-		if($json) {
-			$strava_ride = json_decode($json);
-			
-			//Transform data to a ready to be displayed format
-			$startDate = date("F j, Y - H:i a", strtotime($strava_ride->ride->startDateLocal));
-			$elapsedTime = date("H:i:s", mktime(0, 0, $strava_ride->ride->elapsedTime));
-			$movingTime = date("H:i:s", mktime(0, 0, $strava_ride->ride->movingTime));
-			
-			if ($systemOfMeasurement == "metric") {
-				//To km
-				$distance = number_format($strava_ride->ride->distance/1000, 2);
-				//To km/h
-				$averageSpeed = number_format($strava_ride->ride->averageSpeed * 3.6, 2);
-				//To km/h
-				// Removed on version 2 of the Strava API
-				//$maximumSpeed = number_format($strava_ride->ride->maximumSpeed/1000, 2);
-				//It is already in meters
-				$elevationGain = number_format($strava_ride->ride->elevationGain, 2);
-			} elseif ($systemOfMeasurement == "english") {
-				//To miles
-				$distance = number_format($strava_ride->ride->distance/1609.34, 2);
-				//To miles/h
-				$averageSpeed = number_format($strava_ride->ride->averageSpeed * 2.2369, 2);
-				//To miles/h
-				// Removed on version 2 of the Strava API
-				//$maximumSpeed = number_format($strava_ride->ride->maximumSpeed/1609.34, 2);
-				//To foot
-				$elevationGain = number_format($strava_ride->ride->elevationGain/0.3048, 2);
-			}
-			
-			$ride_details = array(
-				'id' => $rideId,
-				'name' => $strava_ride->ride->name,
-				'athleteId' => $strava_ride->ride->athlete->id,
-				'athleteName' => $strava_ride->ride->athlete->name,
-				'athleteUserName' => $strava_ride->ride->athlete->username,
-				'startDate' => $startDate,
-				'elapsedTime' => $elapsedTime,
-				'movingTime' => $movingTime,
-				'distance' => $distance,
-				'averageSpeed' => $averageSpeed,
-				//'maximumSpeed' => $maximumSpeed,
-				'elevationGain' => $elevationGain
-			);
-			return $ride_details;
-		} else {
-			$this->feedback .= _e("Could not get information from strava.com for the ride id: ") . $stravaRide->id;
-			return false;
-		}
+	const RIDES_URL = "http://app.strava.com/rides/";
+	const ATHLETES_URL = "http://app.strava.com/athletes/";
+	
+	public function getRide( $rideId ) {
+		return WPStrava::get_instance()->api->get( "activities/{$rideId}" );
 	} // getRideDetails
 	
-	public function getRidesDetails($systemOfMeasurement) {
-		if($this->stravaRides) {
-			$rides_details = array();
-			foreach($this->stravaRides as $stravaRide) {
-				$rides_details[] = $this->getRideDetails($stravaRide->id, $systemOfMeasurement);
-			}
-			return $rides_details;
-		} else {
-			$this->feedback .= _e("Please provide the rides array to be processed.", "wp-strava");
-			return false;
-		}
-	} // getRidesDetails
-	
-	public function getLatestRides($searchOption, $searchId, $quantity) {
-		$url = $this->ridesUrl;
-		//Get the json results using the constructor specified values.
-		if($searchOption == "athlete") {
-			if(is_numeric($searchId)) {
-				$json = file_get_contents($url . '?athleteId=' . urlencode($searchId));
-			} else {
-				$json = file_get_contents($url . '?athleteName=' . urlencode($searchId));
-			}
-		} elseif ($searchOption == "club" AND is_numeric($searchId)) {
-			$json = file_get_contents($url . '?clubId=' . urlencode($searchId));
-		} else {
-			$this->feedback .= _e("There's an error on the widget options combination.", "wp-strava");
-		}
-		if($json) {
-			$strava_rides = json_decode($json);
-			$this->stravaRides = array_slice($strava_rides->rides, 0, $quantity);
-		} else {
-			$this->feedback .= _e("There was an error pulling data of strava.com.", "wp-strava");
-			return false;
-		}
-	} // getLatestRides
-	
-	public function getAuthenticationToken($email, $password) {
-		require_once WPSTRAVA_PLUGIN_DIR . 'lib/Util.class.php';
-		$util = new WPStrava_Util();
-		$data = array('email' => $email, 'password' => $password);
-		$json = $util->makePostRequest($this->authenticationUrlV2, $data);
+	public function getRides( $club_id = NULL, $quantity = NULL ) {
+		$api = WPStrava::get_instance()->api;
 
-		if($json) {
-			$strava_login = json_decode($json);
-			if(!isset($strava_login->error)) {
-				$this->feedback .= __('Successfully authenticated.', 'wp-strava');
-				return $strava_login->token;
-			} else {
-				$this->feedback .= __('Authentication failed, please check your credentials.', 'wp-strava');
-				return false;
-			}
+		$data = NULL;
+
+		$args = $quantity ? array( 'per_page' => $quantity ) : NULL;
+		
+		//Get the json results using the constructor specified values.
+		if ( is_numeric( $club_id ) ) {
+			$data = $api->get( "clubs/{$club_id}/activities", $args );
 		} else {
-			$this->feedback .= __('There was an error pulling data of strava.com.', 'wp-strava');
-			return false;
+			$data = $api->get( 'athlete/activities', $args );
 		}
-  } // getAuthenticationToken
-    
-    public function getRideMap($rideId, $token, $efforts, $threshold) {
-    	if($rideId != 0 AND $token != "") {
-    		$url = preg_replace('/:id/', $rideId, $this->rideMapDetailsUrlV2);
-    		$json = file_get_contents($url . '?token=' . $token . '&threshold=' . $threshold);
-    		
-    		if($json) {
-    			//$map_details = json_decode($json);
-    			//return $map_details;
-    			return $json;
-    		} else {
-    			$this->feedback .= _e("There was an error pulling data of strava.com.", "wp-strava");
-				return false;
-    		}
-    	} else {
-    		$this->feedback .= _e("You need to provide both parameters to complete the call.", "wp-strava");
-			return false;
-    	}
-    } // getRideDetails
+
+		if ( is_wp_error( $data ) )
+			return $data;
+		
+		if ( is_array( $data ) )
+			return $data;
+		
+		return array();	
+		
+	} // getRides
+
+	public function getRidesLongerThan( $rides, $dist ) {
+		$som = WPStrava_SOM::get_som();		
+		$meters = $som->distance_inverse( $dist );
+
+		$long_rides = array();
+		foreach ( $rides as $ride_info ) {
+			if ( $ride_info->distance > $meters ) {
+				$long_rides[] = $ride_info;
+			}
+		}
+		
+		return $long_rides;
+	}
+	
 } // class Rides
 ?>
--- lib/Strava.class.php
+++ lib/Strava.class.php
@@ -1,35 +1,75 @@
 <?php
 
 require_once WPSTRAVA_PLUGIN_DIR . 'lib/Settings.class.php';
+require_once WPSTRAVA_PLUGIN_DIR . 'lib/SOM.class.php';
 require_once WPSTRAVA_PLUGIN_DIR . 'lib/LatestRidesWidget.class.php';
+require_once WPSTRAVA_PLUGIN_DIR . 'lib/LatestMapWidget.class.php';
+require_once WPSTRAVA_PLUGIN_DIR . 'lib/RideShortcode.class.php';
+require_once WPSTRAVA_PLUGIN_DIR . 'lib/StaticMap.class.php';
 
 class WPStrava {
 
 	private static $instance = NULL;
 	private $settings = NULL;
+	private $api = NULL;
+	private $rides = NULL;
 	
 	private function __construct() {
 		$this->settings = new WPStrava_Settings();
 
 		if ( is_admin() ) {
 			$this->settings->hook();
+		} else {
+			add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
 		}
 
 		// Register StravaLatestRidesWidget widget
-		add_action( 'widgets_init', function() {	return register_widget( 'WPStrava_LatestRidesWidget' ); } );
-		
+		add_action( 'widgets_init', create_function('', 'return register_widget( "WPStrava_LatestRidesWidget" );') );
+		add_action( 'widgets_init', create_function('', 'return register_widget( "WPStrava_LatestMapWidget" );' ) );
 	}
 
 	public static function get_instance() {
-		if ( ! self::$instance )
-			self::$instance = new WPStrava();
+		if ( ! self::$instance ) {
+			$class = __CLASS__;
+			self::$instance = new $class();
+		}
 		return self::$instance;
 	}
 
 	public function __get( $name ) {
+		//on-demand classes
+		if ( $name == 'api' )
+			return $this->get_api();
+
+		if ( $name == 'rides' )
+			return $this->get_rides();
+
 		if ( isset( $this->{$name} ) )
 			return $this->{$name};
 
 		return NULL;
+	}
+
+	public function get_api() {
+		if ( ! $this->api ) {
+			require_once WPSTRAVA_PLUGIN_DIR . 'lib/API.class.php';
+			$this->api = new WPStrava_API( get_option('strava_token') );
+		}
+
+		return $this->api;
+	}
+	
+	public function get_rides() {
+		if ( ! $this->rides ) {
+			require_once WPSTRAVA_PLUGIN_DIR . 'lib/Rides.class.php';
+			$this->rides = new WPStrava_Rides();
+		}
+
+		return $this->rides;
+	}
+
+	public function register_scripts() {
+		// Register a personalized stylesheet
+		wp_register_style( 'wp-strava-style', WPSTRAVA_PLUGIN_URL . 'css/wp-strava.css' );
 	}
 }
