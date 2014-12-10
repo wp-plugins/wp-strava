354b4c10df63594fe2b0ca870e6160b2f4110e21
jrfoell <jrfoell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-10 03:47:04 +0000

    Added missing classes
    git-svn-id: https://plugins.svn.wordpress.org/wp-strava/trunk@1041643 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/API.class.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * Util is a class with all the utility methods.
+ */
+class WPStrava_API {
+
+	//deactivated
+	//const STRAVA_V1_API = 'http://www.strava.com/api/v1/'; //rides?athleteId=134698
+	//const STRAVA_V2_API = 'http://www.strava.com/api/v2/'; //rides/:ride_id/map_details
+	const STRAVA_V3_API = 'https://www.strava.com/api/v3/';
+
+	public function __construct( $access_token ) {
+		$this->access_token = $access_token;
+	}
+	
+	public function post( $uri, $data = NULL ) {
+		$url = self::STRAVA_V3_API;
+
+		$args = array(
+			'body' => http_build_query( $data ),
+			'sslverify' => false,
+			'headers' => array(
+				'Authorization' => 'Bearer ' . $this->access_token,
+			)
+		);
+
+		$response = wp_remote_post( $url . $uri, $args );
+
+		if ( $response['response']['code'] != 200 ) {
+			//see if there's useful info in the body
+			$body = json_decode( $response['body'] );
+			$error = '';
+			if ( ! empty( $body->error ) )
+				$error = $body->error;
+			else
+				$error = print_r( $response, true );
+
+			return new WP_Error( 'wp-strava_post',
+								 sprintf( __( 'ERROR %s %s - %s', 'wp-strava'), $response['response']['code'], $response['response']['message'], $error ),
+								 $response );
+		}
+
+		return json_decode( $response['body'] );
+	}
+
+	public function get( $uri, $args = NULL ) {
+		$url = self::STRAVA_V3_API;
+
+		$url .= $uri;
+
+		if ( ! empty( $args ) )
+			$url = add_query_arg( $args, $url );
+
+		$get_args = array(
+			'headers' => array(
+				'Authorization' => 'Bearer ' . $this->access_token,
+			)
+		);
+		
+		$response = wp_remote_get( $url, $get_args );
+
+		if ( is_wp_error( $response ) )
+			return $response;
+		
+		if ( $response['response']['code'] != 200 ) {
+			//see if there's useful info in the body
+			$body = json_decode( $response['body'] );
+			$error = '';
+			if ( ! empty( $body->error ) )
+				$error = $body->error;
+			else if ( $response['response']['code'] == 503 )
+				$error = __( 'Strava Temporarily Unavailable', 'wp-strava' );
+			else
+				$error = print_r( $response, true );
+
+			return new WP_Error( 'wp-strava_get',
+								 sprintf( __( 'ERROR %s %s - %s', 'wp-strava'), $response['response']['code'], $response['response']['message'], $error ),
+								 $response );
+		}
+
+		return json_decode( $response['body'] );
+	}
+	
+} // class API
---
+++ lib/LatestMapWidget.class.php
@@ -0,0 +1,116 @@
+<?php
+
+class WPStrava_LatestMapWidget extends WP_Widget {
+
+	private $som;
+	
+	public function __construct() {
+		$this->som = WPStrava_SOM::get_som();
+		
+		parent::__construct(
+	 		false,
+			'Strava Latest Map', // Name
+			array( 'description' => __( 'Strava latest ride using static google map image', 'wp-strava' ), ) // Args
+		);
+	}
+	
+	public function form( $instance ) {
+		// outputs the options form on admin
+        $distance_min = isset( $instance['distance_min'] ) ? esc_attr( $instance['distance_min'] ) : '';
+		$strava_club_id = isset( $instance['strava_club_id'] ) ? esc_attr( $instance['strava_club_id'] ) : '';
+
+		//provide some defaults
+        //$ride_index_params = $ride_index_params ? $ride_index_params : 'athleteId=21';
+
+		?>
+        <p>
+			<label for="<?php echo $this->get_field_id( 'distance_min' ); ?>"><?php echo sprintf( __( 'Min. Distance (%s):', 'wp-strava' ), $this->som->get_distance_label() ); ?></label> 
+        	<input class="widefat" id="<?php echo $this->get_field_id( 'distance_min' ); ?>" name="<?php echo $this->get_field_name( 'distance_min' ); ?>" type="text" value="<?php echo $distance_min; ?>" />
+        </p>
+			<p>
+				<label for="<?php echo $this->get_field_id('strava_club_id'); ?>"><?php _e('Club ID (leave blank to show Athlete):'); ?></label> 
+				<input class="widefat" id="<?php echo $this->get_field_id('strava_club_id'); ?>" name="<?php echo $this->get_field_name('strava_club_id'); ?>" type="text" value="<?php echo $strava_club_id; ?>" />
+			</p>
+        <?php		
+	}
+	
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved from the admin
+		$instance = $old_instance;
+		$instance['strava_club_id'] = strip_tags( $new_instance['strava_club_id'] );
+		$instance['distance_min'] = strip_tags( $new_instance['distance_min'] );
+        return $instance;
+	}
+
+	public function widget( $args, $instance ) {
+		extract( $args );
+		$distance_min = $instance['distance_min'];
+		$strava_club_id = empty( $instance['strava_club_id'] ) ? NULL : $instance['strava_club_id'];
+		$build_new = false;
+		
+		//try our transient first
+		$ride_transient = get_transient( 'strava_latest_map_ride' );
+		$ride_option = get_option( 'strava_latest_map_ride' );
+
+		if ( $ride_transient )
+			$ride = $ride_transient;
+
+		if ( ! $ride ) {
+			$strava_rides = WPStrava::get_instance()->rides;
+			$rides = $strava_rides->getRides( $strava_club_id );
+
+			if ( is_wp_error( $rides ) ) {
+				echo $before_widget;
+				if ( WPSTRAVA_DEBUG ) {
+					echo '<pre>';
+					print_r($rides);
+					echo '</pre>';
+				} else {
+					echo $rides->get_error_message();
+				}
+				echo $after_widget;
+				return;
+			}
+
+			if ( ! empty( $rides ) ) {
+			
+				if ( ! empty( $distance_min ) )
+					$rides = $strava_rides->getRidesLongerThan( $rides, $distance_min );
+
+				$ride = current( $rides );
+
+				//update transients & options
+				if ( $ride->id != $ride_option->id ) {
+					$build_new = true;
+					update_option( 'strava_latest_map_ride', $ride );
+				}
+
+				if ( $ride->id != $ride_transient->id )
+					set_transient( 'strava_latest_map_ride', $ride, 60 * 60 ); //one hour
+			}
+		}
+
+		if ( $ride ):			
+			echo $before_widget;
+			?><h3 class="widget-title">Latest Ride</h3>
+			<a title="<?php echo $ride->name ?>" href="http://app.strava.com/activities/<?php echo $ride->id ?>"><?php
+			echo $this->getStaticImage( $ride->id, $build_new );
+			?></a><?php
+			echo $after_widget;
+		endif;
+	}
+
+
+	private function getStaticImage( $ride_id, $build_new ) {
+		$img = get_option( 'strava_latest_map' );
+		
+		if ( $build_new || ! $img ) {
+			$ride = WPStrava::get_instance()->rides->getRide( $ride_id );
+			$img = WPStrava_StaticMap::get_image_tag( $ride );
+			update_option( 'strava_latest_map', $img );
+		}
+
+		return $img;
+	}
+	
+}
